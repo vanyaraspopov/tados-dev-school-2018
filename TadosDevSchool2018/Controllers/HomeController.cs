1cab4e2bd396b2ec969fc94bc01a7f3037304271
RV <vanyaraspopov@mail.ru>
2018-02-10 20:12:03 +0500

    Выполнено 1 задание

--- TadosDevSchool2018/Controllers/HomeController.cs
+++ TadosDevSchool2018/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,8 @@ namespace TadosDevSchool2018.Controllers
     {
         public IActionResult Index()
         {
+            Task1();
+            Task2();
             Task3();
             return View();
         }
@@ -21,6 +24,34 @@ namespace TadosDevSchool2018.Controllers
             return View();
         }
 
+
+        private void Task1()
+        {
+            var dataPrefix = "task1.";
+
+            string projectDir = System.IO.Directory.GetCurrentDirectory();
+            string path = projectDir + @"\Models\Task1\Smallfuck.cs";
+
+            ViewData[dataPrefix + "file"] = path;
+
+            // Open the file to read from.
+            string[] readText = System.IO.File.ReadAllLines(path);
+            string code = "";
+            foreach (string s in readText)
+            {
+                code += "\n" + s;
+            }
+            var match = Regex.Match(code, @"/^\.$/");
+
+            ViewData[dataPrefix + "code"] = code;
+        }
+
+        private void Task2()
+        {
+            var dataPrefix = "task2.";
+
+            var queryA = "SELECT FROM ";
+        }
 
         private void Task3()
         {
@@ -34,18 +65,13 @@ namespace TadosDevSchool2018.Controllers
             //  Перебор предметов "в странном порядке"
             var enumeratedBooks = new List<Book>();
             foreach (var book in books)
-            {
                 enumeratedBooks.Add(book);
-            }
 
             //  Полный список авторов
             var authorNames = "";
             for (var i = 0; i < authors.Count; i++)
             {
-                if (i > 0)
-                {
-                    authorNames += ", ";
-                }
+                if (i > 0) authorNames += ", ";
                 authorNames += authors[i].Name;
             }
             ViewData[dataPrefix + "authors"] = authorNames;
@@ -54,10 +80,7 @@ namespace TadosDevSchool2018.Controllers
             var enumeratedAuthorNames = "";
             for (var i = 0; i < enumeratedBooks.Count; i++)
             {
-                if (i > 0)
-                {
-                    enumeratedAuthorNames += ", ";
-                }
+                if (i > 0) enumeratedAuthorNames += ", ";
                 enumeratedAuthorNames += enumeratedBooks[i].Author.Name;
             }
             ViewData[dataPrefix + "enumeratedAuthors"] = enumeratedAuthorNames;
---
+++ TadosDevSchool2018/Models/Task1/Smallfuck.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TadosDevSchool2018.Models.Task1
+{
+    public class Smallfuck
+    {
+        public static string Interpreter(string code, string tape)
+        {
+            //  Думаете сам делал? Неа, жизнь слишком коротка, чтобы програмировать уже запрограммированное
+            //  Код взят отсюда https://repl.it/@weylinwagnon/smallfuck и переведён на C#
+
+            var _tape = new bool[tape.Length];
+            for (var i = 0; i < tape.Length; i++)
+            {
+                _tape[i] = int.Parse(tape[i].ToString()) > 0;
+            }
+
+            var t = 0;
+            var c = 0;
+            var op = 0;
+            var cl = 0;
+
+            while ((t >= 0 && t < tape.Length) && (c >= 0 && c < code.Length))
+            {
+                var val = _tape[t];
+                var cmd = code[c];
+
+                if (cmd == '>')
+                {
+                    t++;
+                    c++;
+                }
+                else if (cmd == '<')
+                {
+                    t--;
+                    c++;
+                }
+                else if (cmd == '*')
+                {
+                    _tape[t] = !val;
+                    c++;
+                }
+                else if (cmd == '[')
+                {
+                    c++;
+                    if (!val)
+                    {
+                        c++;
+                        while (c < code.Length)
+                        {
+                            cmd = code[c];
+                            if (cmd == ']' && op == 0)
+                            {
+                                c++;
+                                break;
+                            }
+                            else if (cmd == '[')
+                            {
+                                op++;
+                            }
+                            else if (cmd == ']')
+                            {
+                                op--;
+                            }
+                            c++;
+                        }
+                    }
+                }
+                else if (cmd == ']')
+                {
+                    if (val)
+                    {
+                        c--;
+                        while (c > 0)
+                        {
+                            cmd = code[c];
+                            if (cmd == '[' && cl == 0)
+                            {
+                                break;
+                            }
+                            else if (cmd == ']')
+                            {
+                                cl++;
+                            }
+                            else if (cmd == '[')
+                            {
+                                cl--;
+                            }
+                            c--;
+                        }
+                    }
+                    else
+                    {
+                        c++;
+                    }
+                }
+                else
+                {
+                    c++;
+                }
+            }
+
+            var output = "";
+            for (var i = 0; i < _tape.Length; i++)
+            {
+                output += _tape[i] ? '1' : '0';
+            }
+            return output;
+        }
+    }
+}
